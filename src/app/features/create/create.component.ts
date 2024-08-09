5b6fa5e77e48d9881850ff6cf89bf51643b0ed25
DevOtaviano <otavianovenancio24@gmail.com>
2024-08-09 11:58:16 -0300

    refactor: cria componente de formulário para aumentar a reusabilidade

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,27 +1,15 @@
 import { Component, inject } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
 import { _MatInternalFormField } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ProductsService } from '../../shared/services/products.service';
 import { Router } from '@angular/router';
+import { FormComponent } from '../../shered/components/form/form.component';
+import { Product } from '../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-  ],
+  imports: [FormComponent],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss',
 })
@@ -30,21 +18,10 @@ export class CreateComponent {
   matSnackBar = inject(MatSnackBar);
   router = inject(Router);
 
-  form = new FormGroup({
-    title: new FormControl<string>('', {
-      nonNullable: true,
-      validators: Validators.required,
-    }),
-  });
-
-  onSubmit() {
-    this.productsService
-      .post({
-        title: this.form.controls.title.value,
-      })
-      .subscribe(() => {
-        this.matSnackBar.open('Produto criado com sucesso!', 'Ok');
-      });
+  onSubmit(product: Product) {
+    this.productsService.post(product).subscribe(() => {
+      this.matSnackBar.open('Produto criado com sucesso!', 'Ok');
+    });
     this.router.navigateByUrl('/');
   }
 }
--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -1,23 +1,17 @@
-import { SnapshotOptions } from './../../../../node_modules/@tufjs/models/dist/snapshot.d';
 import { Product } from './../../shared/interfaces/product.interface';
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router, ActivatedRouteSnapshot, ActivatedRoute } from '@angular/router';
+import {
+  Router,
+  ActivatedRoute,
+} from '@angular/router';
 import { ProductsService } from '../../shared/services/products.service';
+import { FormComponent } from '../../shered/components/form/form.component';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-  ],
+  imports: [FormComponent],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss',
 })
@@ -28,21 +22,10 @@ export class EditComponent {
 
   product: Product = inject(ActivatedRoute).snapshot.data['product'];
 
-  form = new FormGroup({
-    title: new FormControl<string>(this.product.title, {
-      nonNullable: true,
-      validators: Validators.required,
-    }),
-  });
-
-  onSubmit() {
-    this.productsService
-      .put(this.product.id, {
-        title: this.form.controls.title.value,
-      })
-      .subscribe(() => {
-        this.matSnackBar.open('Produto editado com sucesso!', 'Ok');
-      });
+  onSubmit(product: Product) {
+    this.productsService.put(this.product.id, product).subscribe(() => {
+      this.matSnackBar.open('Produto editado com sucesso!', 'Ok');
+    });
     this.router.navigateByUrl('/');
   }
 }
