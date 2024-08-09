5f9130df8cd46509d0f1f793b9a058533b1f71c0
DevOtaviano <otavianovenancio24@gmail.com>
2024-08-09 16:01:18 -0300

    refactor: melhora código de ListComponent

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,8 +1,8 @@
 import { Product } from './../../shared/interfaces/product.interface';
 import { ProductsService } from './../../shared/services/products.service';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CardComponent } from './components/card/card.component';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { filter } from 'rxjs';
 import { ComfirmationDialogService } from '../../shared/services/comfirmation-dialog.service';
@@ -15,18 +15,11 @@ import { ComfirmationDialogService } from '../../shared/services/comfirmation-di
   styleUrl: './list.component.scss',
 })
 export class ListComponent {
-  products: Product[] = [];
+  products = signal<Product[]>(inject(ActivatedRoute).snapshot.data['products']);
 
   productsService = inject(ProductsService);
   router = inject(Router);
-
   comfirmationDialogService = inject(ComfirmationDialogService);
-
-  ngOnInit() {
-    this.productsService.getAll().subscribe((products) => {
-      this.products = products;
-    });
-  }
 
   onEdit(product: Product) {
     this.router.navigate(['/edit-product', product.id]);
@@ -39,7 +32,7 @@ export class ListComponent {
       .subscribe(() => {
         this.productsService.delete(product.id).subscribe(() => {
           this.productsService.getAll().subscribe((products) => {
-            this.products = products;
+            this.products.set(products);
           });
         });
       });
